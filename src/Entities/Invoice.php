1f1f4a603c9eac0c1ec4240ce5caff3c5d83b516
Thomas Georgel <thom.georgel@gmail.com>
2024-02-05 14:49:31 +0100

    Revert "Invoices discounts are an array, not an int"

--- src/Entities/Invoice.php
+++ src/Entities/Invoice.php
@@ -49,7 +49,7 @@ class Invoice extends Entity implements Contracts\HasCustomFields, Contracts\Has
 
     public ?array $taxes;
 
-    public ?array $discount;
+    public ?int $discount;
 
     public ?int $fiscal_year_id;
 
